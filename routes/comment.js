c3e678885fa467da0e39ede6c3166f2b9af044ef
Meilyn22 <samsonidoko3@byui.edu>
2022-07-09 15:14:04 -0600

    Correction

--- routes/comment.js
+++ routes/comment.js
@@ -3,11 +3,13 @@ const routes = require('.');
 const router = express.Router();
 
 const commentController = require('../controller/comment');
+const postController = require('../controller/post');
+const userController = require('../controller/user');
 
-routes.get("/:id", commentController.getPostById)
+routes.get("/:id", postController.getPostById)
 routes.get("/:id", commentController.getCommentByID)
 routes.post("/", commentController.addComment )
-routes.get("/:id", commentController.getUserByID )
+routes.get("/:id", userController.getUserByID )
 routes.delete("/:id", commentController.deleteComment )
 routes.put("/:id", commentController.updateComment )
 

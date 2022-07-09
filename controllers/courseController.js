b3eb0c4c578550d85e06b30e71dca2a5a810df29
Bryson Prince <bryson.prince@yahoo.ca>
2022-07-09 14:03:50 -0700

    course controller stubbed out

---
+++ controllers/courseController.js
@@ -0,0 +1,17 @@
+
+// ToDo:
+const addCourse = {};
+const getAllCourses = {};
+const getCourseTitle = {};
+const getCourseId = {};
+const updateCourse = {}; 
+const deleteCourse = {};
+
+module.exports = {
+    addCourse,
+    getAllCourses,
+    getCourseTitle,
+    getCourseId,
+    updateCourse,
+    deleteCourse
+};

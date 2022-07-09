35b35ff5b7cfdf5d384add9cfc5a795e46ed8442
MarcusBYUI <ojo18002@byui.edu>
2022-07-09 21:59:12 +0100

    added post route and model

---
+++ routes/post.js
@@ -0,0 +1,22 @@
+const routes = require("express").Router();
+const {
+  getAllPost,
+  getPostById,
+  getPostByTitle,
+  getPostByCategory,
+  getPostByUser,
+  addPost,
+  updatePost,
+  deletePost,
+} = require("../controller/post");
+
+routes.get("/", getAllPost);
+routes.get("/:id", getPostById);
+routes.get("/title/:title", getPostByTitle);
+routes.get("/category/:category", getPostByCategory);
+routes.get("/user/:user", getPostByUser);
+routes.post("/", addPost);
+routes.put("/", updatePost);
+routes.delete("/", deletePost);
+
+module.exports = routes;

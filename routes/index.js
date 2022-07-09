891dd276aa426671f61ed4a80641759f32bac8a0
MarcusBYUI <ojo18002@byui.edu>
2022-07-09 21:38:47 +0100

    db connection and server startup

---
+++ routes/index.js
@@ -0,0 +1,53 @@
+const routes = require("express").Router();
+const createError = require("http-errors");
+//const UsersRoute = require("./users");
+const swaggerUi = require("swagger-ui-express");
+//const swaggerDocument = require("./swagger-output.json");
+//const authRoute = require("./auth");
+
+//express static
+//routes.use(express.static(path.join(__dirname, "../views")));
+
+//Home route
+routes.get("/", (req, res) => {
+  // #swagger.description = 'API home route'
+  // const user = req.user ? req.user : {};
+  // res.render("index", { user: user });
+
+  res.json({
+    name: "Blog API",
+    version: "1.0.0",
+    description: "",
+    Author: "",
+  });
+});
+
+//auth
+//routes.use("/auth", authRoute);
+
+//products route
+//routes.use("/products", productsRoute);
+
+//users route
+//routes.use("/users", UsersRoute);
+
+//api route
+//routes.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+//404 error handler
+routes.use((req, res, next) => {
+  next(createError.NotFound("Not Found"));
+});
+
+//error handler
+routes.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
+
+module.exports = routes;
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongodb = require("./db/connect");
+const cors = require("cors");
+const Routes = require("./routes");
+//const passportSetup = require("./config/passport-setup");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+//init express and middlewares
+const app = express();
+app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
+// app.use(
+//   cookieSession({
+//     maxAge: 24 * 60 * 60 * 1000,
+//     keys: [process.env.keys],
+//   })
+// );
+//passport
+//app.enable("trust proxy");
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+//routes
+app.use("/", Routes);
+
+//start server and db
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(PORT);
+    console.log(`Connected to DB and listening on ${PORT}`);
+  }
+});

ae2769cb51f0c2df560041aec36a6680eb398c20
Lusyluh <mav19003@byui.edu>
2022-07-14 04:23:19 +0200

    add comment

--- controller/comment.js
+++ controller/comment.js
@@ -1,6 +1,28 @@
+const Comment = require('../models/comment');
+const createError = require("http-errors");
+
+
 const getCommentByPostId = () => {};
 const getCommentByID = () => {};
-const addComment = () => {};
+
+//create a comment
+const addComment = async (req, res,next) => {
+  try {
+    const comment = new Comment({content:req.body.content});
+    await comment.save()
+    res.json({
+      status: 200,
+      message: "New comment added",
+    });
+  } catch (error) {
+    if (error.name == "ValidationError") {
+      next(createError.UnprocessableEntity(error.message));
+      return;
+    }
+    next(error);
+  }
+
+  };
 const getCommentByUserID = () => {};
 const deleteComment = () => {};
 const updateComment = () => {};
--- routes/comment.js
+++ routes/comment.js
@@ -6,7 +6,9 @@ const commentController = require("../controller/comment");
 
 routes.get("/postid/:id", commentController.getCommentByPostId);
 routes.get("/:id", commentController.getCommentByID);
-routes.post("/", commentController.addComment);
+
+routes.post("/comment", commentController.addComment);
+
 routes.get("/userid/:id", commentController.getCommentByUserID);
 routes.delete("/:id", commentController.deleteComment);
 routes.put("/:id", commentController.updateComment);

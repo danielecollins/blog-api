e7252e0841e6de8898c25d01ddf7d71c29f94b4c
Lusyluh <mav19003@byui.edu>
2022-07-14 05:41:20 +0200

    gets comment associated with specified id

--- controller/comment.js
+++ controller/comment.js
@@ -1,15 +1,37 @@
+const mongoose = require("mongoose");
 const Comment = require('../models/comment');
 const createError = require("http-errors");
+const comment = require('../models/comment');
+//const ObjectId = require('mongoose').ObjectId;
+
+
 
 
 const getCommentByPostId = () => {};
-const getCommentByID = () => {};
+const getCommentByID = async (req, res, next) => {
+  try {
+
+    const comment = await Comment.findById(req.params.id);
+
+    if (!comment) {
+      next(createError(422, "Comment does not exist"));
+      return;
+    }
+    res.status(200).json(comment);
+  } catch (error) {
+    if (error instanceof mongoose.CastError) {
+      next(createError(422, "Invalid comment ID"));
+      return;
+    }
+    next(error);
+  }
+};
 
 //create a comment
 const addComment = async (req, res,next) => {
   try {
-    const comment = new Comment({content:req.body.content});
-    await comment.save()
+    const newComment = new Comment({content:req.body.content});
+    await newComment.save()
     res.json({
       status: 200,
       message: "New comment added",

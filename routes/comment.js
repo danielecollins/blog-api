3d9d983d160dfa2fb963a5dc9a87c566c471cccf
MarcusBYUI <ojo18002@byui.edu>
2022-07-17 00:10:44 +0100

    isuser fix

--- routes/comment.js
+++ routes/comment.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const createError = require("http-errors");
+const Comment = require("../models/user");
 
 const routes = express.Router({
   mergeParams: true,
@@ -16,12 +17,14 @@ const isAuth = (req, res, next) => {
 };
 
 const isUser = async (req, res, next) => {
-  if (req.user.id === req.params.id) {
+  const comment = await Comment.findOne({ userId: req.params.id });
+
+  if (req.user.id === comment.userId) {
     next();
   } else if (req.user.level === "admin") {
     next();
   } else {
-    res.send("You can only manage a profile that belongs to you");
+    res.send("You can only manage a comment that belongs to you");
   }
 };
 

5207f3937d5400b7974f3dd376ff4f02f18160eb
MarcusBYUI <ojo18002@byui.edu>
2022-07-16 23:38:04 +0100

    added route restriction

--- routes/comment.js
+++ routes/comment.js
@@ -15,11 +15,21 @@ const isAuth = (req, res, next) => {
   }
 };
 
+const isUser = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    next();
+  } else if (req.user.level === "admin") {
+    next();
+  } else {
+    res.send("You can only manage a profile that belongs to you");
+  }
+};
+
 routes.get("/postId/:postId", commentController.getCommentByPostId);
 routes.get("/:id", commentController.getCommentByID);
 routes.post("/", isAuth, commentController.addComment);
 routes.get("/userid/:id", isAuth, commentController.getCommentByUserID);
-routes.delete("/:id", isAuth, commentController.deleteComment);
-routes.put("/:id", isAuth, commentController.updateComment);
+routes.delete("/:id", isAuth, isUser, commentController.deleteComment);
+routes.put("/:id", isAuth, isUser, commentController.updateComment);
 
 module.exports = routes;

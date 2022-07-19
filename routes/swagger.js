b727f747795a68c176c313854b6251cad70694bf
danielecollins <57637750+danielecollins@users.noreply.github.com>
2022-07-19 14:03:13 -0600

    Took out comment to fix the swagger.js

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 
-// Switch the swagger document to swagger-output local.json to run documentation locally
 const swaggerDocument = require('../swagger-output.json');
 
 router.use('/api-docs', swaggerUi.serve);
